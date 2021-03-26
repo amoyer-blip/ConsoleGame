7988be35618463a8de77751b4c787c246fe2ea50
amoyer-blip <75403219+amoyer-blip@users.noreply.github.com>
2021-03-26 17:16:01 -0400

    Add project files.

---
+++ ConsoleGame/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+
+namespace ConsoleGame
+{
+    class Player
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        private string PlayerMarker;
+        private ConsoleColor PlayerColor;
+
+        public Player(int initialX, int initialY)
+        {
+            X = initialX;
+            Y = initialY;
+            PlayerMarker = "0";
+            PlayerColor = ConsoleColor.Magenta;
+
+        }
+
+        public void Draw()
+        {
+            Console.ForegroundColor = PlayerColor;
+            Console.SetCursorPosition(X, Y);
+            Console.Write(PlayerMarker);
+            Console.ResetColor();
+        }
+    }
+}
---
+++ ConsoleGame/World.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+
+namespace ConsoleGame
+{
+    class World
+    {
+        private string[,] Grid;
+        private int Rows;
+        private int Cols;
+
+        public World(string[,] grid)
+        {
+            Grid = grid;
+            Rows = Grid.GetLength(0);
+            Cols = Grid.GetLength(1);
+        }
+
+        public void Draw()
+        {
+            for (int y = 0; y < Rows; y++)
+            {
+                for (int x = 0; x < Cols; x++)
+                {
+                    string element = Grid[y, x];
+                    Console.SetCursorPosition(x, y);
+                    //Write(element);
+
+                    if (element == "X")
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                    }
+
+                    Console.Write(element);
+                }
+
+            }
+        }
+        //Tracking if "grid position" is empty or not. Empty = okay to move, otherwise blocked. 
+        public string GetElementAt(int x, int y)
+        {
+            return Grid[y, x];
+        }
+
+        public bool IsPositionWalkable(int x, int y)
+        {
+            //Check bounds to ensure x and y are actually in the grid 
+            if(x < 0 || y < 0 || x>= Cols || y>= Rows)
+            {
+                return false;
+            }
+
+            //Check if the grid is a walkable tile. 
+            return Grid[y, x] == " " || Grid[y, x] == "X";
+        }
+    }
+}
